5cb4bdda93cdc4e013f3a7407b92fddee0cfaad9
dbc2201 <divyanshb22@gmail.com>
2020-02-27 08:34:49 +0530

    create the main class and the main method

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: dbc2201
+ *   Date: 27/02/20
+ *   Time: 8:34 AM
+ */
+
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
