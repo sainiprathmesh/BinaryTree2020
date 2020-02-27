2403e442f0139731f9dace61e5af8f6a02c6ad7a
dbc2201 <divyanshb22@gmail.com>
2020-02-27 08:36:36 +0530

    create getter and setter methods for the fields of the Node class

--- src/definition/BinaryTree.java
+++ src/definition/BinaryTree.java
@@ -23,5 +23,29 @@ public class BinaryTree<E> implements BinaryTreeADT<E> {
         public Node(E data) {
             this.data = data;
         }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(Node<E> leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public Node<E> getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(Node<E> rightChild) {
+            this.rightChild = rightChild;
+        }
     }
 }
